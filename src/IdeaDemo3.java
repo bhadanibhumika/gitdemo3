a8630cd915380e9dfefb564b60f3be84172a878e
bhumika <bhumikabhadani69@gmail.com>
2023-03-12 16:27:19 +0000

    added hello commit

--- src/IdeaDemo3.java
+++ src/IdeaDemo3.java
@@ -1,6 +1,7 @@
 public class IdeaDemo3 {
-    public static void main(String[]args){
+    public static void main(String[] args){
         System.out.println("Hello git");
+        System.out.println("Hello commit");
     }
 
 }
